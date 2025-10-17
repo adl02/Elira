c7b32eb197c1b674b43b6bc2c419bdb074ee1ad7
Adil Jawed <mdadiljawed786@gmail.com>
2025-10-17 12:31:57 +0530

    cart page

---
+++ app/src/main/java/com/howtokaise/elira/presentation/page/CartPage.kt
@@ -0,0 +1,10 @@
+package com.howtokaise.elira.presentation.page
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CartPage(modifier: Modifier = Modifier) {
+    Text("cart page")
+}
