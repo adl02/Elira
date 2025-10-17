5230e5cb190ef07fc81d180b591a93a20682633d
Adil Jawed <mdadiljawed786@gmail.com>
2025-10-17 12:31:46 +0530

    profile page

---
+++ app/src/main/java/com/howtokaise/elira/presentation/page/ProfilePage.kt
@@ -0,0 +1,11 @@
+package com.howtokaise.elira.presentation.page
+
+import android.provider.ContactsContract.Profile
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun ProfilePage(modifier: Modifier = Modifier) {
+    Text("profile page")
+}
