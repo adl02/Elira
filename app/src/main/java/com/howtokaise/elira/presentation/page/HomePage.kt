a8c887964ed643558e5df28aa2d633b1579c3ae7
Adil Jawed <mdadiljawed786@gmail.com>
2025-10-17 14:54:20 +0530

    homepage updated

--- app/src/main/java/com/howtokaise/elira/presentation/page/HomePage.kt
+++ app/src/main/java/com/howtokaise/elira/presentation/page/HomePage.kt
@@ -1,10 +1,26 @@
 package com.howtokaise.elira.presentation.page
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.howtokaise.elira.presentation.components.BannerView
+import com.howtokaise.elira.presentation.components.HeaderView
 
 @Composable
 fun HomePage(modifier: Modifier = Modifier) {
-    Text("home page")
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ){
+        HeaderView(modifier)
+        Spacer(modifier = Modifier.height(10.dp))
+        BannerView(modifier.height(200.dp))
+    }
 }
