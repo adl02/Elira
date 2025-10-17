480bde5a796134821b568fab3f75b1a8431de72c
Adil Jawed <mdadiljawed786@gmail.com>
2025-10-17 11:34:41 +0530

    navigation

--- app/src/main/java/com/howtokaise/elira/presentation/navigation/AppNavigation.kt
+++ app/src/main/java/com/howtokaise/elira/presentation/navigation/AppNavigation.kt
@@ -5,27 +5,36 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.google.firebase.Firebase
+import com.google.firebase.auth.auth
 import com.howtokaise.elira.presentation.authentication.AuthScreen
 import com.howtokaise.elira.presentation.authentication.LoginScreen
 import com.howtokaise.elira.presentation.authentication.SignupScreen
+import com.howtokaise.elira.presentation.homescreen.HomeScreen
 
 @Composable
 fun AppNavigation(modifier: Modifier = Modifier) {
 
     val navController = rememberNavController()
+    val isLoggedIn = Firebase.auth.currentUser!=null
+    val firstPage = if (isLoggedIn) "home" else "auth"
 
-    NavHost(navController = navController, startDestination = "auth") {
+    NavHost(navController = navController, startDestination = firstPage) {
 
         composable("auth"){
             AuthScreen(modifier,navController)
         }
 
         composable("signup"){
-            SignupScreen(modifier)
+            SignupScreen(modifier,navController)
         }
 
         composable("login"){
-            LoginScreen(modifier)
+            LoginScreen(modifier,navController)
+        }
+
+        composable("home"){
+            HomeScreen(modifier,navController)
         }
     }
 }
