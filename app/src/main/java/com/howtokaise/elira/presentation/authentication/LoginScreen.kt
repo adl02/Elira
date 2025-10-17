a386f5a265c489163590ea1ec39dab51ea843054
Adil Jawed <mdadiljawed786@gmail.com>
2025-10-17 11:32:35 +0530

    login completed

--- app/src/main/java/com/howtokaise/elira/presentation/authentication/LoginScreen.kt
+++ app/src/main/java/com/howtokaise/elira/presentation/authentication/LoginScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,21 +22,30 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.howtokaise.elira.AppUtil
 import com.howtokaise.elira.R
+import com.howtokaise.elira.presentation.viewmodel.AuthViewmodel
 
 @Composable
-fun LoginScreen(modifier: Modifier = Modifier) {
+fun LoginScreen(modifier: Modifier = Modifier, navController: NavController, authViewmodel: AuthViewmodel= viewModel()) {
     val isDarkTheme = isSystemInDarkTheme()
     val backgroundColor = if (isDarkTheme) Color.Black else Color.White
 
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+
+    var isLoading by remember { mutableStateOf(false) }
+
+    var context = LocalContext.current
 
     Column(
         modifier = Modifier
@@ -96,12 +106,30 @@ fun LoginScreen(modifier: Modifier = Modifier) {
         Spacer(modifier = Modifier.height(20.dp))
 
         Button(
-            onClick = {},
+            onClick = {
+                isLoading = true
+                authViewmodel.login(email,password){success, errorMessage->
+                    if (success){
+                        isLoading = false
+                        navController.navigate("home"){
+                            popUpTo("auth"){inclusive = true}
+                        }
+                    }else{
+                        isLoading = false
+                        AppUtil.showToast(context,errorMessage?: "Something went wrong")
+                    }
+                }
+            },
+            enabled = !isLoading,
             modifier = Modifier
                 .fillMaxWidth()
                 .height(60.dp)
         ) {
-            Text("Login",fontSize = 20.sp)
+            if (isLoading){
+                CircularProgressIndicator()
+            }else{
+                Text("Login",fontSize = 20.sp)
+            }
         }
     }
 }
