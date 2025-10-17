49235d81bb2cf09f89abd3b6c7cba5ae9460ad63
Adil Jawed <mdadiljawed786@gmail.com>
2025-10-17 12:31:34 +0530

    favorite page

---
+++ app/src/main/java/com/howtokaise/elira/presentation/page/FavoritePage.kt
@@ -0,0 +1,10 @@
+package com.howtokaise.elira.presentation.page
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun FavouritePage(modifier: Modifier = Modifier) {
+    Text("favourite page")
+}
