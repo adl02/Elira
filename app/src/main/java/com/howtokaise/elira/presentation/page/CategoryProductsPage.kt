b2e53563c8d88861bf2b6cf7a963109beb38a1bc
Adil Jawed <mdadiljawed786@gmail.com>
2025-10-19 02:04:22 +0530

    CategoryProductsPage added

--- app/src/main/java/com/howtokaise/elira/presentation/page/CategoryProductsPage.kt
+++ app/src/main/java/com/howtokaise/elira/presentation/page/CategoryProductsPage.kt
@@ -1,12 +1,49 @@
 package com.howtokaise.elira.presentation.page
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
+import androidx.compose.ui.unit.dp
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
+import com.howtokaise.elira.model.ProductModel
 
 @Composable
 fun CategoryProductsPage(modifier: Modifier = Modifier, categoryId : String) {
-    Text(text = "Category products page " +categoryId)
 
+    val productList = remember { mutableStateOf<List<ProductModel>>(emptyList()) }
+
+    LaunchedEffect(Unit) {
+        Firebase.firestore.collection("data")
+            .document("stock")
+            .collection("products")
+            .whereEqualTo("category",categoryId)
+            .get().addOnCompleteListener {
+                if (it.isSuccessful) {
+                    val resultList = it.result.documents.mapNotNull { doc ->
+                        doc.toObject(ProductModel::class.java)
+                    }
+                    productList.value = resultList
+                }
+            }
+    }
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize()
+            .padding(16.dp)
+    ) {
+        items(productList.value){item ->
+            Text(text = item.title)
+            Spacer(modifier = Modifier.height(20.dp))
+        }
+    }
 }
