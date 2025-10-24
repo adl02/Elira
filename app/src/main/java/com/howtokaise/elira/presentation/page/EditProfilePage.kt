e3c90efe55ebf3cf4305134b4560e13c1b795361
Adil Jawed <mdadiljawed786@gmail.com>
2025-10-25 03:21:41 +0530

    editProfile page

---
+++ app/src/main/java/com/howtokaise/elira/presentation/page/EditProfilePage.kt
@@ -0,0 +1,9 @@
+package com.howtokaise.elira.presentation.page
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun EditProfilePage(modifier: Modifier = Modifier) {
+
+}
