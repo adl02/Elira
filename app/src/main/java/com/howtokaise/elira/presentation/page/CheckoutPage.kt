e02848f6ea41a50649c010aee1358ad379363480
Adil Jawed <mdadiljawed786@gmail.com>
2025-10-25 21:57:02 +0530

    checkout updated

--- app/src/main/java/com/howtokaise/elira/presentation/page/CheckoutPage.kt
+++ app/src/main/java/com/howtokaise/elira/presentation/page/CheckoutPage.kt
@@ -1,6 +1,7 @@
 package com.howtokaise.elira.presentation.page
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -10,15 +11,21 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -31,6 +38,7 @@ import com.google.firebase.firestore.firestore
 import com.howtokaise.elira.AppUtil
 import com.howtokaise.elira.model.ProductModel
 import com.howtokaise.elira.model.UserModel
+import com.howtokaise.elira.presentation.navigation.GlobalNavigation
 
 @Composable
 fun CheckoutPage(modifier: Modifier = Modifier) {
@@ -91,53 +99,72 @@ fun CheckoutPage(modifier: Modifier = Modifier) {
         modifier = modifier
             .fillMaxSize()
             .background(backgroundColor)
-            .padding(16.dp)
     ) {
-        Text(text = "Checkout", fontSize = 22.sp, fontWeight = FontWeight.Bold)
-        Spacer(modifier = Modifier.height(16.dp))
 
-        Text(text = "Deliver to:", fontWeight = FontWeight.Bold, fontSize = 18.sp)
-        Spacer(Modifier.height(4.dp))
-        Text(text = address.fullName, fontWeight = FontWeight.Medium)
-        Text(text = address.phone)
-        Text(text = "${address.roadName}, ${address.city}, ${address.state} - ${address.pincode}")
-
-        Spacer(modifier = Modifier.height(16.dp))
-        HorizontalDivider()
-        Spacer(modifier = Modifier.height(16.dp))
-        RowCheckoutItems(title = "Subtotal", value = subTotal.value.toString())
-        Spacer(modifier = Modifier.height(8.dp))
-        RowCheckoutItems(title = "Discount (-)", value = discount.value.toString())
-        Spacer(modifier = Modifier.height(8.dp))
-        RowCheckoutItems(title = "Tax (+)", value = tax.value.toString())
-        HorizontalDivider()
-
-        Spacer(Modifier.height(16.dp))
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = "To Pay",
-            textAlign = TextAlign.Center
-        )
-        Text(
-            modifier = Modifier.fillMaxWidth(),
-            text = "₹" + total.value.toString(),
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold,
-            textAlign = TextAlign.Center
-        )
-
-        Spacer(Modifier.height(16.dp))
-
-        Button(
-            onClick = {
-                AppUtil.startPayment(total.value)
-            },
+        Row(
             modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp),
-            colors = ButtonDefaults.buttonColors(Color(0xFF349137))
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
         ) {
-            Text(text = "Pay Now", color = Color.White, fontSize = 16.sp)
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = "Back",
+                modifier = Modifier
+                    .size(30.dp)
+                    .clickable { GlobalNavigation.navController.popBackStack() }
+            )
+            Spacer(modifier = Modifier.width(12.dp))
+            Text(
+                text = "Checkout", fontSize = 22.sp, fontWeight = FontWeight.Bold
+            )
+        }
+
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            Text(text = "Deliver to:", fontWeight = FontWeight.Bold, fontSize = 18.sp)
+            Spacer(Modifier.height(4.dp))
+            Text(text = address.fullName, fontWeight = FontWeight.Medium)
+            Text(text = address.phone)
+            Text(text = "${address.roadName}, ${address.city}, ${address.state} - ${address.pincode}")
+
+            Spacer(modifier = Modifier.height(16.dp))
+            HorizontalDivider()
+            Spacer(modifier = Modifier.height(16.dp))
+            RowCheckoutItems(title = "Subtotal", value = subTotal.value.toString())
+            Spacer(modifier = Modifier.height(8.dp))
+            RowCheckoutItems(title = "Discount (-)", value = discount.value.toString())
+            Spacer(modifier = Modifier.height(8.dp))
+            RowCheckoutItems(title = "Tax (+)", value = tax.value.toString())
+            HorizontalDivider()
+
+            Spacer(Modifier.height(16.dp))
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "To Pay",
+                textAlign = TextAlign.Center
+            )
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "₹" + total.value.toString(),
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(Modifier.height(16.dp))
+
+            Button(
+                onClick = {
+                    AppUtil.startPayment(total.value)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(Color(0xFF349137))
+            ) {
+                Text(text = "Pay Now", color = Color.White, fontSize = 16.sp)
+            }
         }
     }
 }
