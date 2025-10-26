79611ab0ea692e134c930292081b513a44501027
Adil Jawed <mdadiljawed786@gmail.com>
2025-10-27 04:00:30 +0530

    BackHandler behavior added

--- app/src/main/java/com/howtokaise/elira/presentation/homescreen/HomeScreen.kt
+++ app/src/main/java/com/howtokaise/elira/presentation/homescreen/HomeScreen.kt
@@ -1,5 +1,7 @@
 package com.howtokaise.elira.presentation.homescreen
 
+import android.widget.Toast
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
@@ -17,15 +19,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import com.howtokaise.elira.presentation.page.CartPage
 import com.howtokaise.elira.presentation.page.HomePage
 import com.howtokaise.elira.presentation.page.ProfilePage
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier, navController: NavController) {
@@ -37,9 +45,32 @@ fun HomeScreen(modifier: Modifier = Modifier, navController: NavController) {
     )
 
     var selectedIndex by rememberSaveable { mutableStateOf(0) }
+    var backPressedOnce by rememberSaveable { mutableStateOf(false) }
+    val context = LocalContext.current
+    val coroutineScope = rememberCoroutineScope()
+    var backPressJob: Job? = remember { null }
 
     val isDarkTheme = isSystemInDarkTheme()
     val backgroundColor = if (isDarkTheme) Color.Black else Color.White
+
+    BackHandler {
+        if (selectedIndex != 0) {
+            selectedIndex = 0 // Go to Home
+        } else {
+            if (backPressedOnce) {
+                // Exit app
+                android.os.Process.killProcess(android.os.Process.myPid())
+            } else {
+                backPressedOnce = true
+                Toast.makeText(context, "Press back again to exit", Toast.LENGTH_SHORT).show()
+                backPressJob?.cancel()
+                backPressJob = coroutineScope.launch {
+                    delay(2000)
+                    backPressedOnce = false
+                }
+            }
+        }
+    }
 
     Scaffold(
         bottomBar = {
